021e87dcffef9551037ad9e71a4438f344cb11ce
zenoskongfu <zenos.chen@foxmail.com>
2025-04-21 00:12:30 +0800

    update code

--- app/get-static-params/[title]/[subtitle]/page.tsx
+++ app/get-static-params/[title]/[subtitle]/page.tsx
@@ -1,14 +1,26 @@
-export const generateStaticParams = async (options: {
-	params: { title: string };
-}) => {
+export const generateStaticParams = async (options: { params: { title: string } }) => {
 	const { title } = options.params;
 	console.log("title: ", title);
 	return [{ subtitle: title + "hello" }, { subtitle: title + "world" }];
 };
 
-export default async function Page(props: {
-	params: Promise<{ subtitle: string }>;
-}) {
+async function getData() {
+	// 接口每次调用都会返回一个随机的猫猫图片数据
+	const res = await fetch("https://api.thecatapi.com/v1/images/search", {
+		cache: "no-store",
+	});
+	return res.json();
+}
+
+export default async function Page(props: { params: Promise<{ subtitle: string }> }) {
 	const { subtitle } = await props.params;
-	return <div>subTitle: {subtitle}</div>;
+	const data = await getData();
+	return (
+		<div>
+			subTitle: {subtitle}
+			<div>
+				<img src={data[0].url} alt='' />
+			</div>
+		</div>
+	);
 }
